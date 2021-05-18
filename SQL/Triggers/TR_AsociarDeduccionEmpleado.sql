0f2a8185aebc9c3e2ce31dbc29ccc38be2e7dbf4
Luis Araya Mata <luistec2309@estudiantec.cr>
2021-05-18 17:23:09 -0600

    Avance del Trigger

---
+++ SQL/Triggers/TR_AsociarDeduccionEmpleado.sql
@@ -0,0 +1,21 @@
+USE SistemaObrero
+
+CREATE TRIGGER  TR_AsociarDeduccionEmpleado
+
+ON Empleado
+
+FOR INSERT
+
+AS
+
+DECLARE @IdEmpleado INT
+	= ( SELECT Id 
+	FROM dbo.Empleado AS E 
+	WHERE 
+		Id = (SELECT IDENT_CURRENT('Empleado')));
+
+
+EXECUTE sp_AsignarDeduccionesEmpleado  @IdEmpleado,0;
+
+
+
